bea706c36b8c16d7b27dba29f243c4ed5bf0e4a0
marcin <marcinlimanski@gmail.com>
2023-08-18 11:20:31 -0400

    init commit

---
+++ contracts/StorageExample.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: UNLICENSED
+
+// Solidity files have to start with this pragma.
+// It will be used by the Solidity compiler to validate its version.
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
